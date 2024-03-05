ad977067e9b87de04768c3c53d66dcf5c1278422
Yurii P <valetkrest@gmail.com>
2024-03-05 14:39:24 +0200

    add grpc request for login registration and refresh token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -11,11 +11,56 @@ import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
 import { RegistrationDto } from './dto/registration.dto';
 import { RefreshDto } from './dto/refresh.dto';
+import { GrpcMethod, RpcException } from '@nestjs/microservices';
+import { Status } from '@grpc/grpc-js/build/src/constants';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  // gRPC
+  @GrpcMethod('UsersService', 'RegistrationUser')
+  async registrationGRPC(dto: RegistrationDto) {
+    const candidate = await this.authService.getUser({ email: dto.email });
+    if (candidate) {
+      throw new RpcException({ code: Status.ALREADY_EXISTS });
+    }
+    const newUser = await this.authService.addUser(dto);
+    if (!newUser) {
+      throw new RpcException({ code: Status.INTERNAL });
+    }
+    return { message: 'success' };
+  }
+
+  @GrpcMethod('UsersService', 'LoginUser')
+  async loginGRPC(dto: AuthDto) {
+    console.log(dto);
+    const user = await this.authService.getUser({ email: dto.email });
+    if (!user) {
+      throw new RpcException({ code: Status.NOT_FOUND });
+    }
+    try {
+      await this.authService.passwordCompare(dto.password, user.password);
+      const tokenPair = await this.authService.getNewTokenPair(user);
+      return { tokenPair };
+    } catch (error) {
+      throw new RpcException({ code: Status.INVALID_ARGUMENT });
+    }
+  }
+
+  @GrpcMethod('UsersService', 'RefreshToken')
+  async refreshGRPC(dto: RefreshDto) {
+    const user = await this.authService.getUser({
+      refreshToken: dto.refreshToken,
+    });
+    if (!user) {
+      throw new RpcException({ code: Status.INVALID_ARGUMENT });
+    }
+    const tokenPair = await this.authService.getNewTokenPair(user);
+    return { tokenPair };
+  }
+
+  // REST
   @Post('registration')
   @UsePipes(new ValidationPipe())
   async registration(@Body() dto: RegistrationDto) {
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from './pipes/validation.pipe';
 import * as dotenv from 'dotenv';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   dotenv.config();
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.GRPC,
+    options: {
+      package: 'commentator', // name package proto file
+      protoPath: join(__dirname, 'protos/commentator.proto'),
+      url: 'localhost:4000',
+    },
+  });
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
+  await app.startAllMicroservices();
   await app.listen(3000);
 }
 bootstrap();
